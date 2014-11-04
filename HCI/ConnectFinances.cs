c2aa582cb8a3b4522e80d1c0872d945d197d4c81
pappdavid27 <pappdavid27@gmail.com>
2014-11-04 23:47:41 +0100

    saját commit

--- HCI/ConnectFinances.cs
+++ HCI/ConnectFinances.cs
@@ -11,11 +11,15 @@ namespace HCI
     {
 
         string connectionString;
+        int Balance;
+        int TypedBalance;
+        IEnumerable<Finances> AllFinances;
 
         public ConnectFinances()
         {
             connectionString = @" Data Source = ..."; // ide kell majd a DB elérése
-            
+            Balance = 0;
+            TypedBalance = 0;
         }
 
         public void InsertRecord(Finances f) // hozzáad gombra hívódik
@@ -52,13 +56,40 @@ namespace HCI
             }
         }
 
-        public int Total() // visszaadja az egyenleget
+        public void RefreshBalance() // visszaadja az egyenleget
+        {
+            
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.RunInTransaction(() =>
+                    {
+                        // Balance = conn.Query<Finances>("SELECT SUM(Amount) FROM Finances").ElementAt(0).Amount;
+                        Balance = conn.Table<Finances>().Sum(v => v.Amount);
+                    });
+            }
+
+        }
+
+        public void RefreshTypedBalance(string Type)
         {
             using (var conn = new SQLiteConnection(connectionString))
             {
                 conn.RunInTransaction(() =>
                     {
-                        conn.Query();
+                        // TypedBalance = conn.Query<Finances>("SELECT SUM(Amount) FROM Finances WHERE Type = ?", Type).ElementAt(0).Amount;
+                        TypedBalance = conn.Table<Finances>().Where(v1=>v1.Type == Type).Sum(v2 => v2.Amount);
+                    });
+            }
+        }
+
+        public void GetAllRecords()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.RunInTransaction(() =>
+                    {
+                        // AllFinances = conn.Query<Finances>("SELECT * FROM Finances");
+                        AllFinances = conn.Table<Finances>();
                     });
             }
         }
