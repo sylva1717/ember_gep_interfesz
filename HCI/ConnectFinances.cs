dc2512099f727963704e5435d71bd94c47ebc083
pappdavid27 <pappdavid27@gmail.com>
2014-11-04 22:06:38 +0100

    finances és connectfiannces osztályok + sqlite.cs

---
+++ HCI/ConnectFinances.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI
+{
+    class ConnectFinances
+    {
+        SQLiteConnection conn;
+        string connectionString;
+
+        public ConnectFinances()
+        {
+            connectionString = @" Data Source = ..."; // ide kell majd a DB elérése
+            conn = new SQLiteConnection(connectionString);
+            
+        }
+
+        public void InsertRecord(Finances f) // hozzáad gombra hívódik
+        {
+
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.RunInTransaction(()=>
+                    {
+                        conn.Insert(new Finances() {Id=f.Id, Date=f.Date, Amount=f.Amount, Type=f.Type});
+                    });
+            }
+        }
+
+        public void DeleteRecord(int ID) // kukába húzásra hívódik
+        {
+
+        }
+
+        public int MaxId() // visszaadja a max Id-t a táblában
+        {
+            return 0;
+        }
+
+        public int Total() // visszaadja az egyenleget
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ HCI/Finances.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI
+{
+    class Finances
+    {
+        private int id;
+        private DateTime date;
+        private int amount;
+        private string type;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+    }
+}
